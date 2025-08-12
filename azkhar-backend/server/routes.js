4de240915f9b4fbfe0d707e398258d4d3a328662
Amjad Ahmad <amjad.qano3@gmail.com>
2025-08-12 11:42:44 +0300

    Create Local DB

--- azkhar-backend/server/routes.js
+++ azkhar-backend/server/routes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const dbConnection = require("./database/connection.js");
 
 router.get("/", async (req, res) => {
-  const pool = req.app.get("pool");
+  const pool = dbConnection;
   try {
     const result = await pool.query(
       "SELECT id, username, email FROM userschema.users"
     );
     res.json(result.rows);
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Server error");
+    console.error("DB query error:", error);
+    res.status(500).json({ error: error.message || "Internal Server Error" });
   }
 });
 
