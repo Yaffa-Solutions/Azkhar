d20c6f3d95204e098466c5eac80c464e35ee2bd1
Amjad Ahmad <amjad.qano3@gmail.com>
2025-08-12 13:07:51 +0300

    Move routes to index.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const router = require("./routes.js");
+const router = require("./controllers");
 
 const app = express();
 app.use(express.json());
