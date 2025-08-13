1d8ddcea57c6e02a8eed8b300caffc7f5ee7474d
Jenan04 <jenanyasser1@gmail.com>
2025-08-13 11:32:23 +0300

    all routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-
+const dbConnection = require("./database/config/connection");
 const router = require("./controllers");
+// const azkar = require("./controllers/adamin");
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,9 @@ app.use(cors());
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(router);
+app.use( router);
+
+
+
 
 module.exports = app;
